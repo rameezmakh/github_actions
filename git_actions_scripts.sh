6f26dd2640f746b99ca42f9b0e3ecbeddf57fee8
rameezmakh <makhdoomi.rameez@gmail.com>
2021-12-23 14:58:19 +0530

    Create git_actions_scripts.sh

---
+++ git_actions_scripts.sh
@@ -0,0 +1,11 @@
+          echo What is in this directory?
+          ls -a
+          echo
+          echo IS Java Installed?
+          java --version
+          echo IS Git Installed?
+          git --version
+          echo Check php version
+          php --version
+          echo who is running this program
+          whoami
